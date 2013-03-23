b26774ad8de9e58e2c146b3cf22b0127111c89a9
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2013-03-23 15:47:03 +0100

    Version geaendert

--- version.h
+++ version.h
@@ -16,7 +16,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define d_emu64_version 5.0.9
-#define str_emu64_version "5.0.9 Dev"
+#define d_emu64_version 5.0.8
+#define str_emu64_version "5.0.8 Dev"
 
 #endif // VERSION_H
