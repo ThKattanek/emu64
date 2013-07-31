ae05215246141bf5bec80d3f3b92cbc88b0ec5cd
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2013-08-01 00:36:02 +0200

    - Versionsnummer auf 5.0.9 gesetzt

--- version.h
+++ version.h
@@ -8,7 +8,7 @@
 // Dieser Sourcecode ist Copyright geschützt!   //
 // Geistiges Eigentum von Th.Kattanek           //
 //                                              //
-// Letzte Änderung am 23.03.2013                //
+// Letzte Änderung am 01.08.2013                //
 // www.emu64.de                                 //
 //                                              //
 //////////////////////////////////////////////////
@@ -16,7 +16,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define d_emu64_version 5.0.8
-#define str_emu64_version "5.0.8 Dev"
+#define d_emu64_version 5.0.9
+#define str_emu64_version "5.0.9 Dev"
 
 #endif // VERSION_H
