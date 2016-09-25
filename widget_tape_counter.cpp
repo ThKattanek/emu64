04cd93e61630945d088132e7a9ac84d51209b566
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2016-09-25 11:40:58 +0200

    [ticket#45] WidgetTapeCounter ist Final und sieht nun richtig chick aus :)

--- widget_tape_counter.cpp
+++ widget_tape_counter.cpp
@@ -1,3 +1,18 @@
+//////////////////////////////////////////////////
+//                                              //
+// Emu64                                        //
+// von Thorsten Kattanek                        //
+//                                              //
+// #file: widget_tape_counter.cpp               //
+//                                              //
+// Dieser Sourcecode ist Copyright geschützt!   //
+// Geistiges Eigentum von Th.Kattanek           //
+//                                              //
+// Letzte Änderung am 25.09.2016                //
+// www.emu64.de                                 //
+//                                              //
+//////////////////////////////////////////////////
+
 #include "widget_tape_counter.h"
 #include "ui_widget_tape_counter.h"
 
@@ -6,6 +21,9 @@ WidgetTapeCounter::WidgetTapeCounter(QWidget *parent) :
     ui(new Ui::WidgetTapeCounter)
 {
     ui->setupUi(this);
+
+    pix1 = new QPixmap(":/grafik/counter_numbers_24_384.png");
+    pix3 = new QPixmap(":/grafik/counter_numbers_24_32_blend.png");
 }
 
 WidgetTapeCounter::~WidgetTapeCounter()
@@ -15,5 +33,58 @@ WidgetTapeCounter::~WidgetTapeCounter()
 
 void WidgetTapeCounter::SetCounter(unsigned int count)
 {
-    ui->TestLabel->setText(QVariant(count).toString());
+   this->count = count;
+
+   int off;
+
+   int fine0 = count%100;
+
+   count /= 100;
+   int z0 = count%10;
+   count /= 10;
+   int z1 = count%10;
+   count /= 10;
+   int z2 = count%10;
+
+   int y0 = z0*32 + 32 + ((fine0/100.0)*32.0);
+
+   if(z0==9 ) off = fine0/100.0*32.0;
+   else off = 0;
+   int y1 = z1*32 + 32 + off;
+
+   if(z1==9 && z0==9) off = fine0/100.0*32.0;
+   else off = 0;
+   int y2 = z2*32 + 32 + off;
+
+   pix2 = pix1->copy(0,y0,24,32);
+
+   // Schatten drüberblenden
+   QPainter p0( &pix2 );
+   p0.drawPixmap(0,0,24,32,*pix3);
+   p0.end();
+
+   ui->ziffer0->setPixmap(pix2);
+
+   pix2 = pix1->copy(0,y1,24,32);
+
+   // Schatten drüberblenden
+   QPainter p1( &pix2 );
+   p1.drawPixmap(0,0,24,32,*pix3);
+   p1.end();
+
+   ui->ziffer1->setPixmap(pix2);
+
+   pix2 = pix1->copy(0,y2,24,32);
+
+   // Schatten drüberblenden
+   QPainter p2( &pix2 );
+   p2.drawPixmap(0,0,24,32,*pix3);
+   p2.end();
+
+   ui->ziffer2->setPixmap(pix2);
+}
+
+unsigned int WidgetTapeCounter::GetCounter()
+{
+    return count;
 }
--- widget_tape_counter.h
+++ widget_tape_counter.h
@@ -1,7 +1,23 @@
+//////////////////////////////////////////////////
+//                                              //
+// Emu64                                        //
+// von Thorsten Kattanek                        //
+//                                              //
+// #file: widget_tape_counter.h                 //
+//                                              //
+// Dieser Sourcecode ist Copyright geschützt!   //
+// Geistiges Eigentum von Th.Kattanek           //
+//                                              //
+// Letzte Änderung am 25.09.2016                //
+// www.emu64.de                                 //
+//                                              //
+//////////////////////////////////////////////////
+
 #ifndef WIDGET_TAPE_COUNTER_H
 #define WIDGET_TAPE_COUNTER_H
 
 #include <QWidget>
+#include <QPainter>
 
 namespace Ui {
 class WidgetTapeCounter;
@@ -16,9 +32,16 @@ public:
     ~WidgetTapeCounter();
 
     void SetCounter(unsigned int count);
+    unsigned int GetCounter(void);
 
 private:
     Ui::WidgetTapeCounter *ui;
+
+    unsigned int count;
+
+    QPixmap *pix1;
+    QPixmap pix2;
+    QPixmap *pix3;
 };
 
 #endif // WIDGET_TAPE_COUNTER_H
