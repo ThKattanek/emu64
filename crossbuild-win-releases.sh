c5885cbbd477d16c03bcef12be82227ab3fada99
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2018-01-31 21:19:07 +0100

    [issue#125] qt_de.ts mit eingebunden.

--- crossbuild-win-releases.sh
+++ crossbuild-win-releases.sh
@@ -1,26 +1,44 @@
+# //////////////////////////////////////////////////
+# //                                              //
+# // Emu64                                        //
+# // von Thorsten Kattanek                        //
+# //                                              //
+# // #file: crossbuild-win-releases.sh            //
+# //                                              //
+# // Dieser Sourcecode ist Copyright geschützt!   //
+# // Geistiges Eigentum von Th.Kattanek           //
+# //                                              //
+# // Letzte Änderung am 31.01.2018                //
+# // www.emu64-projekt.de                         //
+# //                                              //
+# //////////////////////////////////////////////////
+
 #!/bin/bash
 
 # script by thorsten kattanek
-# excample: build-windows-release.sh ~/MXE
+# excample: crossbuild-win-releases.sh ~/MXE
 
-declare project_name=sfml-demo
+declare project_name=emu64
+
+# Version
+version=$(git describe --always --tags)
 
 # check of variable $1
 if [ $1 ]; then
-    declare dir=$1
+    declare mxe_path=$1
     # check of exist path from variable $1
-    if [ ! -d $dir ]; then
-        echo "Not exist the path: " $1
+    if [ ! -d $mxe_path ]; then
+        echo "Not exist the MXE path: " $1
         exit
     fi
 else
-    echo "Please specify the MXE path (excample: build-windows-release.sh ~/MXE)"
+    echo "Please specify the MXE path (excample: crossbuild-win-releases.sh ~/MXE)"
     exit
 fi    
 
 # check of cmake for static i686 and x86_64
-declare i686_cmake=$1/usr/bin/i686-w64-mingw32.static-cmake
-declare x86_64_cmake=$1/usr/bin/x86_64-w64-mingw32.static-cmake
+declare i686_cmake=$mxe_path/usr/bin/i686-w64-mingw32.static-cmake
+declare x86_64_cmake=$mxe_path/usr/bin/x86_64-w64-mingw32.static-cmake
 
 if [ ! -e $i686_cmake ]; then
     echo "Cmake for static i686 is not exist!"
@@ -39,7 +57,7 @@ if [ $i686_ok ]; then
     echo "Creating a i686-Static windows binary ..."
     
     # check and create a build dir
-    declare build_i686_dir=./build_win_32bit
+    declare build_i686_dir=./build-$project_name_$version_win_x32
     if [ ! -d $build_i686_dir ]; then
     mkdir $build_i686_dir; 
     else
@@ -47,15 +65,15 @@ if [ $i686_ok ]; then
     fi
     
     # check and delete if exist a install dir
-    declare install_i686_dir=./install_win_32bit
+    declare install_i686_dir=./$project_name_$version_win_x32
     if [ -d $install_i686_dir ]; then
         rm -rf $install_i686_dir/*
     fi
     
     # execute cmake
     cd $build_i686_dir
-    $i686_cmake .. -DSFML_STATIC_LIBRARIES=TRUE -DCMAKE_INSTALL_PREFIX=../$install_i686_dir
-    make install
+    $i686_cmake .. -DCMAKE_INSTALL_PREFIX=../$install_i686_dir -DWIN32_STATIC_BUILD=TRUE
+    make -j8 install
     cd ..
     
     #rm -rf $build_i686_dir
@@ -65,12 +83,14 @@ if [ $i686_ok ]; then
     7z a -t7z -m0=LZMA -mmt=on -mx=9 -md=96m -mfb=256 $install_i686_dir/$project_name-win-32bit.7z $install_i686_dir/*
 fi
 
+exit
+
 ### 64Bit Static
 if [ $x86_64_ok ]; then
     echo "Creating a x86_64-Static windows binary ..."
     
     # check and create a build dir
-    declare build_x86_64_dir=./build_win_64bit
+    declare build_x86_64_dir=./build-win-64bit
     if [ ! -d $build_x86_64_dir ]; then
         mkdir $build_x86_64_dir; 
     else
@@ -78,15 +98,15 @@ if [ $x86_64_ok ]; then
     fi
     
     # check and delete if exist a install dir
-    declare install_x86_64_dir=./install_win_64bit
+    declare install_x86_64_dir=./install-win-64bit
     if [ -d $install_x86_64_dir ]; then
         rm -rf $install_x86_64_dir/*
     fi
     
     # execute cmake
     cd $build_x86_64_dir
-    $x86_64_cmake .. -DSFML_STATIC_LIBRARIES=TRUE -DCMAKE_INSTALL_PREFIX=../$install_x86_64_dir
-    make install
+    $x86_64_cmake .. -DCMAKE_INSTALL_PREFIX=../$install_x86_64_dir -DWIN32_STATIC_BUILD=TRUE
+    make -j8 install
     cd ..
     
     #rm -rf $build_x86_64_dir
