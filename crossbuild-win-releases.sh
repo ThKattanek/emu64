67d5a1c038cccb870b45bb96bfa7337142bfbc0a
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2018-01-31 22:17:30 +0100

    [issue#125] Sript zu erstellen der Windowsversionen ist fertiggestellt.

--- crossbuild-win-releases.sh
+++ crossbuild-win-releases.sh
@@ -36,6 +36,15 @@ else
     exit
 fi    
 
+# check and create a public_release dir
+if [ ! -d ./public_release ]; then
+mkdir ./public_release; 
+else
+    rm -rf ./public_release/*
+fi
+
+cd public_release
+
 # check of cmake for static i686 and x86_64
 declare i686_cmake=$mxe_path/usr/bin/i686-w64-mingw32.static-cmake
 declare x86_64_cmake=$mxe_path/usr/bin/x86_64-w64-mingw32.static-cmake
@@ -57,61 +66,69 @@ if [ $i686_ok ]; then
     echo "Creating a i686-Static windows binary ..."
     
     # check and create a build dir
-    declare build_i686_dir=./build-$project_name_$version_win_x32
+    declare build_i686_dir="./build-"$project_name"_"$version"_win_x32"
     if [ ! -d $build_i686_dir ]; then
     mkdir $build_i686_dir; 
     else
         rm -rf $build_i686_dir/*
     fi
-    
+
     # check and delete if exist a install dir
-    declare install_i686_dir=./$project_name_$version_win_x32
+    declare install_i686_dir="./"$project_name"_"$version"_win_x32"
     if [ -d $install_i686_dir ]; then
         rm -rf $install_i686_dir/*
     fi
-    
+
     # execute cmake
     cd $build_i686_dir
-    $i686_cmake .. -DCMAKE_INSTALL_PREFIX=../$install_i686_dir -DWIN32_STATIC_BUILD=TRUE
+    $i686_cmake ../.. -DCMAKE_INSTALL_PREFIX=../$install_i686_dir -DWIN32_STATIC_BUILD=TRUE
     make -j8 install
     cd ..
     
-    #rm -rf $build_i686_dir
-    
+    rm -rf $build_i686_dir
+
     # compress as 7z
     echo "Release 32bit as 7z kompressed..."
-    7z a -t7z -m0=LZMA -mmt=on -mx=9 -md=96m -mfb=256 $install_i686_dir/$project_name-win-32bit.7z $install_i686_dir/*
-fi
+    7z a -t7z -m0=LZMA -mmt=on -mx=9 -md=96m -mfb=256 $install_i686_dir".7z" $install_i686_dir
 
-exit
+    rm -rf $install_i686_dir
+fi
 
 ### 64Bit Static
 if [ $x86_64_ok ]; then
     echo "Creating a x86_64-Static windows binary ..."
-    
+
     # check and create a build dir
-    declare build_x86_64_dir=./build-win-64bit
+    declare build_x86_64_dir="./build-"$project_name"_"$version"_win_x64"
     if [ ! -d $build_x86_64_dir ]; then
-        mkdir $build_x86_64_dir; 
+    mkdir $build_x86_64_dir;
     else
         rm -rf $build_x86_64_dir/*
     fi
-    
+
     # check and delete if exist a install dir
-    declare install_x86_64_dir=./install-win-64bit
+    declare install_x86_64_dir="./"$project_name"_"$version"_win_x64"
     if [ -d $install_x86_64_dir ]; then
         rm -rf $install_x86_64_dir/*
     fi
-    
+
     # execute cmake
     cd $build_x86_64_dir
-    $x86_64_cmake .. -DCMAKE_INSTALL_PREFIX=../$install_x86_64_dir -DWIN32_STATIC_BUILD=TRUE
+    $x86_64_cmake ../.. -DCMAKE_INSTALL_PREFIX=../$install_x86_64_dir -DWIN32_STATIC_BUILD=TRUE
     make -j8 install
     cd ..
-    
-    #rm -rf $build_x86_64_dir
-  
+
+    rm -rf $build_x86_64_dir
+
     # compress as 7z
     echo "Release 64bit as 7z kompressed..."
-    7z a -t7z -m0=LZMA -mmt=on -mx=9 -md=96m -mfb=256 $install_x86_64_dir/$project_name-win-64bit.7z $install_x86_64_dir/*
+    7z a -t7z -m0=LZMA -mmt=on -mx=9 -md=96m -mfb=256 $install_x86_64_dir".7z" $install_x86_64_dir
+
+    rm -rf $install_x86_64_dir
 fi
+
+cd ..
+
+echo "--> Die fertigen Pakete befinden sich im Verzeichnis 'public_release' !"
+echo ""
+echo "E.N.D.E"
