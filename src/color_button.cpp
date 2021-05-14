ba4f85a9d25d6006cf11d1cc34c4dfef8e5fe464
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2021-05-14 18:17:59 +0200

    ColorButton: Rahmen korrigiert und Farbflaeche hinzugefuegt. SetColor, GetColor und SetColorNumber hinzugefuegt.

--- src/color_button.cpp
+++ src/color_button.cpp
@@ -2,6 +2,7 @@
 #include "ui_color_button.h"
 
 #include <QPainter>
+#include <QPainterPath>
 
 ColorButton::ColorButton(QWidget *parent) :
 	QWidget(parent),
@@ -11,6 +12,8 @@ ColorButton::ColorButton(QWidget *parent) :
 
 	this->setMinimumWidth(50);
 	this->setMinimumHeight(50);
+
+	color_number = 0;
 }
 
 void ColorButton::paintEvent(QPaintEvent *event)
@@ -18,7 +21,28 @@ void ColorButton::paintEvent(QPaintEvent *event)
 	QPainter p;
 	p.begin(this);
 
-	p.drawRoundRect(0,0,this->width()-1, this->height()-1, 5, 5);
+	p.setRenderHint(QPainter::Antialiasing);
+
+	p.drawRoundedRect(0,0,this->width()-1, this->height()-1, 10, 10);
+
+	QPainterPath path;
+	path.addRoundedRect(QRect(5,5,this->width()-11, this->height()-11),5,5);
+	QPen pen(color, 1);
+	p.setPen(pen);
+	p.fillPath(path, color);
+	p.drawPath(path);
+
+	QFont font;
+	font.setPixelSize(24);
+	font.setBold(true);
+
+	QPainterPath path1;
+	path1.addText(6,26, font, QString::number(color_number));
+
+	QPen pen1(Qt::white,1.4);
+	p.setPen(pen1);
+	p.setBrush(Qt::black);
+	p.drawPath(path1);
 
 	p.end();
 }
@@ -26,4 +50,19 @@ void ColorButton::paintEvent(QPaintEvent *event)
 ColorButton::~ColorButton()
 {
 	delete ui;
+}
+
+void ColorButton::SetColor(QColor color)
+{
+	this->color = color;
+}
+
+QColor ColorButton::GetColor()
+{
+	return color;
+}
+
+void ColorButton::SetColorNumber(int color_number)
+{
+	this->color_number = color_number;
 }
--- src/color_button.h
+++ src/color_button.h
@@ -16,8 +16,16 @@ public:
 	void paintEvent(QPaintEvent *event);
 	~ColorButton();
 
+	void SetColor(QColor color);
+	QColor GetColor();
+
+	void SetColorNumber(int color_number);
+
 private:
 	Ui::ColorButton *ui;
+
+	QColor color;
+	int color_number;
 };
 
 #endif // COLOR_BUTTON_H
