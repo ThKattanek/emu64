017dc7df64494d4be3b631ed7ff0454289696fa4
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2021-05-14 18:21:57 +0200

    UserPaletteWindow: Labels in SpinBoxes gewandelt. Alle ColorButtons in ein Array zusammengefasst.

--- src/user_palette_window.cpp
+++ src/user_palette_window.cpp
@@ -14,6 +14,32 @@ UserPaletteWindow::UserPaletteWindow(QWidget *parent) :
 	ui->red_slider->setMaximum(255);
 	ui->green_slider->setMaximum(255);
 	ui->blue_slider->setMaximum(255);
+
+	ui->red_out->setMaximum(255);
+	ui->green_out->setMaximum(255);
+	ui->blue_out->setMaximum(255);
+
+	color_buttons[0] = ui->color_button_0;
+	color_buttons[1] = ui->color_button_1;
+	color_buttons[2] = ui->color_button_2;
+	color_buttons[3] = ui->color_button_3;
+	color_buttons[4] = ui->color_button_4;
+	color_buttons[5] = ui->color_button_5;
+	color_buttons[6] = ui->color_button_6;
+	color_buttons[7] = ui->color_button_7;
+	color_buttons[8] = ui->color_button_8;
+	color_buttons[9] = ui->color_button_9;
+	color_buttons[10] = ui->color_button_10;
+	color_buttons[11] = ui->color_button_11;
+	color_buttons[12] = ui->color_button_12;
+	color_buttons[13] = ui->color_button_13;
+	color_buttons[14] = ui->color_button_14;
+	color_buttons[15] = ui->color_button_15;
+
+	for(int i=0; i<16; i++)
+	{
+		color_buttons[i]->SetColorNumber(i);
+	}
 }
 
 UserPaletteWindow::~UserPaletteWindow()
@@ -21,17 +47,50 @@ UserPaletteWindow::~UserPaletteWindow()
 	delete ui;
 }
 
+void UserPaletteWindow::SetColor(int color_number, QColor color)
+{
+	if(color_number >= 0 && color_number < 16)
+	{
+		color_buttons[color_number]->SetColor(color);
+	}
+}
+
+QColor UserPaletteWindow::GetColor(int color_number)
+{
+	if(color_number >= 0 && color_number < 16)
+	{
+		return color_buttons[color_number]->GetColor();
+	}
+
+	return Qt::black;
+}
+
 void UserPaletteWindow::on_red_slider_valueChanged(int value)
 {
-	ui->red_out->setText(QString::number(value));
+	ui->red_out->setValue(value);
 }
 
 void UserPaletteWindow::on_green_slider_valueChanged(int value)
 {
-	ui->green_out->setText(QString::number(value));
+	ui->green_out->setValue(value);
 }
 
 void UserPaletteWindow::on_blue_slider_valueChanged(int value)
 {
-	ui->blue_out->setText(QString::number(value));
+	ui->blue_out->setValue(value);
+}
+
+void UserPaletteWindow::on_red_out_valueChanged(int arg1)
+{
+	ui->red_slider->setValue(arg1);
+}
+
+void UserPaletteWindow::on_green_out_valueChanged(int arg1)
+{
+	ui->green_slider->setValue(arg1);
+}
+
+void UserPaletteWindow::on_blue_out_valueChanged(int arg1)
+{
+	ui->blue_slider->setValue(arg1);
 }
--- src/user_palette_window.h
+++ src/user_palette_window.h
@@ -2,6 +2,7 @@
 #define USER_PALETTE_WINDOW_H
 
 #include <QDialog>
+#include "./color_button.h"
 
 namespace Ui {
 class UserPaletteWindow;
@@ -15,15 +16,23 @@ public:
 	explicit UserPaletteWindow(QWidget *parent = nullptr);
 	~UserPaletteWindow();
 
+	void SetColor(int color_number, QColor color);
+	QColor GetColor(int color_number);
+
 private slots:
 	void on_red_slider_valueChanged(int value);
-
 	void on_green_slider_valueChanged(int value);
-
 	void on_blue_slider_valueChanged(int value);
+
+	void on_red_out_valueChanged(int arg1);
+
+	void on_green_out_valueChanged(int arg1);
+
+	void on_blue_out_valueChanged(int arg1);
 
 private:
 	Ui::UserPaletteWindow *ui;
+	ColorButton *color_buttons[16];
 };
 
 #endif // USER_PALETTE_WINDOW_H
