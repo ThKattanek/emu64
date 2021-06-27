69e6f4b63ddedf784243f974030248a6be95c19b
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2021-06-27 20:04:44 +0200

    c64_file_types.h hinzugefuegt

---
+++ src/c64_file_types.h
@@ -0,0 +1,17 @@
+#ifndef C64_FILE_TYPES_H
+#define C64_FILE_TYPES_H
+
+enum C64_FILE_TYPES
+{
+	NO_C64_FILE = -1,
+	PRG,
+	C64,
+	T64,
+	P00,
+	D64,
+	G64,
+	TAP,
+	WAV
+};
+
+#endif // C64_FILE_TYPES_H
