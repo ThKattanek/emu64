f77e1d6e31d92db861f7c46d7019d7e687d7339b
Felix Palmen <felix@palmen-it.de>
2023-06-27 19:43:57 +0200

    Fix type in libpng callback
    This is entirely irrelevant for functionality as the argument isn't
    used, but the wrong type is now considered a hard error by LLVM clang
    16, breaking the build.

--- src/savepng.c
+++ src/savepng.c
@@ -25,7 +25,7 @@
 #endif
 
 /* libpng callbacks */ 
-static void png_error_SDL(png_structp *png_ptr, png_const_charp str)
+static void png_error_SDL(png_structp png_ptr, png_const_charp str)
 {
 	SDL_SetError("libpng: %s\n", str);
 }
