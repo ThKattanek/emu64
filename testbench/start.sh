2427756368cd573ae1fc556a2f4e95f2ec82f0f2
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2026-04-12 20:08:33 +0200

    Update start.sh

--- testbench/start.sh
+++ testbench/start.sh
@@ -4,8 +4,8 @@ if [ $1 ]; then
     declare test_name=$1
 fi
 
-declare testbench_path=/home/thorsten/Programmierung/Aktuelle_Projekte/vice-emu-code/testprogs/testbench
-declare emu64_path=/home/thorsten/Programmierung/Aktuelle_Projekte/emu64/build/install_linux/bin
+declare testbench_path=~/Programmierung/Aktuelle_Projekte/_not_from_me/vice-emu-code/testprogs/testbench
+declare emu64_path=~/Programmierung/Aktuelle_Projekte/qtcreator/emu64/build/install_linux/bin
 
 cd ${testbench_path}
-EMUDIR=${emu64_path} ./testbench.sh emu64 ${test_name}
+EMUDIR=${emu64_path} ./testbench.sh emu64 ${test_name} --verbose
